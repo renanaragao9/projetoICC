e671fbd4923535e0ee4e886635e28753822ccf7a
renanaragao9 <renanaragao159@gmail.com>
2024-03-06 15:59:45 -0300

    Criando tela de Consultas

---
+++ js/index.js
@@ -0,0 +1,6 @@
+new window.VLibras.Widget('https://vlibras.gov.br/app');
+    
+function readText(text) {
+    var texto = "127.0.0.1 deseja reproduzir discurso";
+    responsiveVoice.speak(text, "Brazilian Portuguese Female"); // Altere para o idioma e o gênero de voz desejados
+}
